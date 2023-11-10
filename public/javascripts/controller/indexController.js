3ec7c512216445bb601c618891f053f2bd8f641c
isimsizacemi <abdurrahman19ozkan@gmail.com>
2023-11-10 13:48:17 +0300

    username - users - NewUser

--- public/javascripts/controller/indexController.js
+++ public/javascripts/controller/indexController.js
@@ -1,13 +1,27 @@
 
 app.controller('indexController' , ["$scope" , 'indexFactory', ($scope,indexFactory) =>{
-    const connectionOptions = {
-        reconnectionAttemtps:3,
-        reconnectionDelay:600
+    
+    $scope.init = () => {
+        const username = prompt('Please Enter Username');
+
+        if(username)
+            initSocket(username);
+        else 
+            return false;
+
+    };
+    
+    function initSocket(username){
+        const connectionOptions = {
+            reconnectionAttemtps:3,
+            reconnectionDelay:600
+        };
+    
+        indexFactory.connectSocket('http://localhost:3000', connectionOptions)
+        .then((socket)=>{
+            socket.emit('newUser', {username});
+        }).catch((err) => {
+            console.log(err);
+        });
     }
-    indexFactory.connectSocket('http://localhost:3000', connectionOptions)
-    .then((socket)=>{
-        console.log('bağlantı gerçekleşti',socket)
-    }).catch((err) => {
-        console.log(err);
-    });
 } ]);
