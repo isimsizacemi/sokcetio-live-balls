e24882ba5196b2c313936a264a16ab98324361b5
isimsizacemi <abdurrahman19ozkan@gmail.com>
2023-11-11 10:45:09 +0300

    fiveth

--- public/javascripts/controller/indexController.js
+++ public/javascripts/controller/indexController.js
@@ -1,27 +1,77 @@
+app.controller('indexController', ["$scope", 'indexFactory', ($scope, indexFactory) => {
+    $scope.messages = [ ]; // $scope.messages'yı bir dizi olarak başlat
+    $scope.players = { }; 
 
-app.controller('indexController' , ["$scope" , 'indexFactory', ($scope,indexFactory) =>{
-    
+
     $scope.init = () => {
         const username = prompt('Please Enter Username');
 
-        if(username)
+        if (username)
             initSocket(username);
-        else 
+        else
             return false;
-
     };
-    
-    function initSocket(username){
+
+    function initSocket(username) {
         const connectionOptions = {
-            reconnectionAttemtps:3,
-            reconnectionDelay:600
+            reconnectionAttemtps: 3,
+            reconnectionDelay: 600
         };
-    
+
         indexFactory.connectSocket('http://localhost:3000', connectionOptions)
-        .then((socket)=>{
-            socket.emit('newUser', {username});
-        }).catch((err) => {
+        .then((socket) => {
+            socket.emit('newUser', { username });
+
+            socket.on('newUser', (data) => {
+                const messageData = {
+                    type: {
+                        code:0, // server or diis connect olayı 
+                        message:1
+                    }, // info
+                    username: data.username
+                };
+                $scope.messages.push(messageData);
+                $scope.player[data.id] = data;
+                $scope.$apply();
+            });
+
+            socket.on('initPlayers', (players) =>{
+                $scope.players = players;
+                $scope.$apply();
+                console.log($scope.players);
+
+
+            });
+
+            socket.on('disUser', data => {
+                const messageData = {
+                    type:{
+                        code:0,
+                        message:0
+                    },
+                    username:data.username
+                };
+                $scope.messages.push(messageData);
+                delete  $scope.player[data.id];
+                $scope.$apply();
+            }) 
+            
+            let animate = false;
+
+           
+            $scope.onClickPlayer = ($event) => {
+                console.log($event.offsetX, $event.offsetY);
+                if(!animate){
+                    animate = true;
+                    $('#' + socket.id).animate({'left' : $event.offsetX , 'top' : $event.offsetY});
+                    animate = false;
+                }
+                
+            }            
+        })
+        .catch((err) => {
             console.log(err);
         });
+
     }
-} ]);
+}]);
